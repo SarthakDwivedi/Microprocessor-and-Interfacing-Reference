f2d09536fad89a46d7cbc2802f5a02809a4cbb67
Sarthak Dwivedi <sarthakwarlock@gmail.com>
2019-01-20 16:53:09 +0530

    Dynamic Styling

---
+++ Resources/scripts/script.js
@@ -0,0 +1,23 @@
+let root = document.documentElement;
+setInterval(setRandomColor, 500);
+// setInterval(setRandomColor2, 5000);
+
+
+
+
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  function setRandomColor() {
+    root.style.setProperty('--linear-gradient1', getRandomColor());
+  }
+
+  function setRandomColor2() {
+    root.style.setProperty('--linear-gradient2', getRandomColor());
+  }
